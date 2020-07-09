e1752b5aeea90ae80cd64f2196c0c93ec7175915
Süleyman Ekmekçi <63970122+suleymanekmekci@users.noreply.github.com>
2020-07-10 00:26:39 +0300

    Create decompressRLElist.java

---
+++ decompressRLElist.java
@@ -0,0 +1,53 @@
+
+/**We are given a list nums of integers representing a list compressed with run-length encoding.
+
+ Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+
+ Return the decompressed list.
+
+ Example 1:
+
+ Input: nums = [1,2,3,4]
+ Output: [2,4,4,4]
+ Explanation: The first pair [1,2] means we have freq = 1 and val = 2 so we generate the array [2].
+ The second pair [3,4] means we have freq = 3 and val = 4 so we generate [4,4,4].
+ At the end the concatenation [2] + [4,4,4] is [2,4,4,4].
+ Example 2:
+
+ Input: nums = [1,1,2,3]
+ Output: [1,3,3]*/
+class decompressRLElist {
+    public static int[] decompressRLElist(int[] nums) {
+        int length = findLengthOfOutput(nums);
+        int freqIndex = 0;
+        int valIndex = 1;
+        int[] output = new int[length];
+
+
+        int counter = 0;
+        while (valIndex < nums.length) {
+            int tempCounter = counter;
+            int freq = nums[freqIndex];
+            int val = nums[valIndex];
+
+            while (counter < freq + tempCounter) {
+                output[counter] = val;
+                counter++;
+            }
+
+            freqIndex += 2;
+            valIndex += 2;
+        }
+        return output;
+    }
+
+    public static int findLengthOfOutput(int[] nums) {
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (i % 2 == 0 ) {
+                counter += nums[i];
+            }
+        }
+        return counter;
+    }
+}
