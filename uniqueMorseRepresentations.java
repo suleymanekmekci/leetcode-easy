bd6c27893a0af922a0663912aabda41108ebc392
Süleyman Ekmekçi <63970122+suleymanekmekci@users.noreply.github.com>
2020-07-24 00:54:36 +0300

    Create uniqueMorseRepresentations.java

---
+++ uniqueMorseRepresentations.java
@@ -0,0 +1,51 @@
+/**
+ * International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+ *
+ * For convenience, the full table for the 26 letters of the English alphabet is given below:
+ *
+ * [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+ * Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-..--...", (which is the concatenation "-.-." + ".-" + "-..."). We'll call such a concatenation, the transformation of a word.
+ *
+ * Return the number of different transformations among all words we have.
+ *
+ * Example:
+ * Input: words = ["gin", "zen", "gig", "msg"]
+ * Output: 2
+ * Explanation:
+ * The transformation of each word is:
+ * "gin" -> "--...-."
+ * "zen" -> "--...-."
+ * "gig" -> "--...--."
+ * "msg" -> "--...--."
+ *
+ * There are 2 different transformations, "--...-." and "--...--.".
+ * Note:
+ *
+ * The length of words will be at most 100.
+ * Each words[i] will have length in range [1, 12].
+ * words[i] will only consist of lowercase letters.
+ */
+class uniqueMorseRepresentations {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] mors = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        Map<String,String> morsAlphabet = new HashMap<>();
+
+        for (int i = 0; i < mors.length ; i++) {
+            morsAlphabet.put(alphabet[i],mors[i]);
+        }
+
+        Set<String> output = new HashSet<>();
+
+        for (int j = 0; j < words.length; j++) {
+            String wordInMors = "";
+            for (String letter : words[j].split("")) {
+                wordInMors += morsAlphabet.get(letter);
+            }
+
+            output.add(wordInMors);
+        }
+
+        return output.size();
+    }
+}
